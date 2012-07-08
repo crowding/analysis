091343bdc2610aaa93b7f98de6a80e2cc66f13e2
peterm <peterm@2a468891-300a-0410-9f4f-8ca473c5017f>
2012-07-08 13:38:10 +0000

    looking at bicontrast
    git-svn-id: svn+ssh://pat.shadlen.org/home/peterm/svn/eyetracking/analysis/ConcentricDirectionQuest@850 2a468891-300a-0410-9f4f-8ca473c5017f

--- bicontrast_series_calculations.R
+++ bicontrast_series_calculations.R
@@ -13,12 +13,77 @@ suppressPackageStartupMessages({
 `%+%` <- union
 
 main <- function(flist, dbfile, outfile) {
+
   files <- str_trim(readLines(flist))
 
-  flist <- file(outfile, 'w')
-  on.exit(close(flist), add=TRUE)
-  
+  outputs <- file(outfile, 'w')
+  on.exit(close(outputs), add=TRUE)
+
   trials <- pull.from.sqlite(dbfile, data.frame(loaded.from=files))
+  
+  threshes <- measure_thresholds(trials, sims=0, use_folded=TRUE, abs_bias=TRUE)
+  athreshes <- measure_thresholds(trials, sims=0, use_folded=FALSE, abs_bias=FALSE)
+  threshes <- mutate(threshes, contrast = folded_content_with + folded_content_against)
+  athreshes <- mutate(athreshes, contrast = trial_extra_content_cw + trial_extra_content_ccw)
+
+  pdf_file <- replace_extension(outfile, "pdf")
+  writeLines(pdf_file, outputs)
+  pdf(pdf_file, onefile=TRUE)
+  on.exit(dev.off(), add=TRUE)
+  
+  (ggplot(subset(athreshes, abs(yint) < 100))
+   + aes(trial_extra_content_ccw - trial_extra_content_cw, yint, color=contrast, group=contrast, ymin = yint+yint.sd, ymax = yint - yint.sd)
+   + geom_point()
+   + scale_color_gradientn(name="Luminance Contrast\n(CW + CCW)"
+                          , breaks=seq(0.1,0.5, by=0.1)
+                          , colours=c(low="#0088FF", mid="#444444", high="#FFAA00"))
+   + scale_x_continuous(name="Opponent Carrier Motion (CW - CCW)")
+   + scale_y_continuous(name="Bias (CW responses to stationary envelope, log-odds)", breaks=seq(-4,4,2))
+   + facet_grid(subject ~ target_spacing)
+   + geom_line()
+   + geom_errorbar(width=0)
+   + coord_cartesian(ylim=c(-6,6))
+   + geom_hline(y=0)
+   + geom_hline(y=0)
+   + theme_bw()
+   ) -> bias_plot
+  
+  ## (ggplot(subset(threshes, abs(yint) < 100))
+  ##  + aes(contrast, slope, color=
+  ##        , ymin = slope+slope.sd, ymax = slope - slope.sd, group=trial_extra_content_cw - trial_extra_content_ccw)
+  ##  + geom_point()
+  ##  + facet_grid(subject ~ target_spacing, scales="free")
+  ##  + geom_errorbar(width=0)
+  ##  + theme_bw()
+  ##  + scale_y_continuous("Sensitivity to envelope motion", limits=c(-10, 50))
+  ##  + scale_x_continuous("Luminance Contrast\n(CW + CCW)")
+  ##  + scale_color_gradientn(name="Carrier Motion (CW - CCW)"
+  ##                          , colours=c(low="#0088FF", mid="#444444", high="#FFAA00"))
+  ##  + geom_hline(y=0)
+  ##  ) -> slope_plot
+
+
+    (ggplot(subset(threshes, abs(yint) < 100))
+   + aes(contrast, slope, color=folded_content_with - folded_content_against
+         , ymin = slope+slope.sd, ymax = slope - slope.sd, group=folded_content_with - folded_content_against)
+   + geom_point()
+   + facet_grid(subject ~ target_spacing, scales="free")
+   + geom_errorbar(width=0)
+   + theme_bw()
+   + scale_y_continuous("Sensitivity to envelope motion", limits=c(-10, 50))
+   + scale_x_continuous("Luminance Contrast\n(incongruent + congruent)")
+   + scale_color_gradientn(name="Carrier Motion\n(incongruent - congruent)"
+                           , colours=c(low="#0088FF", mid="#444444", high="#FFAA00"))
+   + geom_hline(y=0)
+   ) -> slope_plot
+
+  print(bias_plot)
+  print(slope_plot)
+
+   #Quick gloss!
+   #Bias seems to be controlled by the difference betwen congruent and incongruent carriers.
+   #Sensitivity (to envelope motion) seems to be controlled by the contrast (except that ML is more sensitive to perfectly counterphase motion, but this is a motion energy effect.
+  
 }
 
 if ("--slave" %in% commandArgs()) {
--- data_functions.R
+++ data_functions.R
@@ -5,64 +5,115 @@ suppressPackageStartupMessages({
 `%-%` <- setdiff
 `%+%` <- union
 
-exclude.cols <-  c(  "abs_displacement", "abs_localDirectionContrast"
-                   , "visibilityCondition", "folded_displacement"
+exclude.cols <-  c(  "abs_displacement", "folded_displacement"
+                   , "visibilityCondition"
                    , "folded_response", "abs_response", "trial_extra_nTargets"
                    , "responseInWindow", "responseTime", "maxResponseTime"
                    , "loaded_from", "runs_i")
 
 measure_thresholds <- function(  trials
                                , per_session=FALSE
+                               , use_folded=TRUE
                                , ...
                                , split = colnames(trials) %-% exclude.cols) {
 
   if (per_session) split <- split %+% "loaded_from"
-  used <- c("folded_displacement", "folded_response", "responseInWindow")
-  ddply(trials[split %+% used], split, psychometric_function, .progress="text", ...)
+
+  used <- c("responseInWindow")
+
+  if (use_folded) {
+    split <- split %-% c("abs_localDirectionContrast"
+                         , "trial_extra_content_ccw", "trial_extra_content_cw")
+    used <- used %+% c("folded_displacement", "folded_response")
+  } else {
+    split <- split %-% c("folded_localDirectionContrast"
+                         , "folded_content_with", "folded_content_against")
+    used <- used %+% c("abs_displacement", "abs_response")
+  }
+  
+  ddply(trials, split, psychometric_function, .progress="text",
+        use_folded=use_folded, ...)
 }
 
-psychometric_function <- function(data, average_bias=TRUE, sims=500) {
-  #return slope, threshold, and quantiles of each from simulation
-  data <- mutate(data, response.cw = folded_response > 0)
+has_both_signs <- function(x) diff(range(sign(x))) == 2
 
-  if (average_bias & (n_sessions <- length(unique(data$loaded_from))) > 0) {
+#TODO think about measuring absolute bias....
+
+psychometric_function <- function(  data, average_bias=TRUE, use_folded=TRUE, abs_bias=TRUE
+                                  , sims=500
+                                  , one_sided = use_folded && !has_both_signs(data$folded_displacement)) {
+  cont <- list()
+  
+  #return slope, threshold, and quantiles of each from simulation
+  if (use_folded) {
+    data <- mutate(data, folded_response = folded_response > 0)
+    formula <- folded_response ~ folded_displacement
+    var <- "folded_displacement"
+    if (abs_bias) {
+      ##Try to account for absolute(clockwise/counterclockwise)
+      ##biases. This might improve the estimate we care about.
+
+      ##I can't figure how to interpret intercepts or whatnot here.
+      data <- mutate(data, not_folded = ifelse(abs_displacement == folded_displacement, 0.5, -0.5))
+      formula <- update(formula, . ~ . + not_folded)
+      #cont$not_folded <- contr.sum
+    }
+  } else {
+    data <- mutate(  data, abs_response = abs_response > 0)
+    formula <- abs_response ~ abs_displacement
+    var <- "abs_displacement"
+  }
+
+  
+  if (one_sided) {
+    
+    ##In folded data, we have to assume symmetry; the function must go through 0.
+    formula <- update(formula, . ~ . - 1)
+
+  } else if (average_bias & (n_sessions <- length(unique(data$loaded_from))) > 1) {
     ##we allow the bias to vary by session, and measure the average
     ##bias by specifying the contrast in sum form.  Note that I
     ##could make this more parsimonious by letting there only be one
     ##adjustment between pairs of sessions.
     data <- mutate(data, loaded_from = factor(loaded_from))
-    
-    fit <- glm(  response.cw ~ folded_displacement + loaded_from
-               , binomial(link=logit.2asym(0.05, 0.05))
-               , subset(data, as.logical(responseInWindow))
-               , contrasts=list(loaded_from="contr.sum"))
-  } else {
-    
-    fit <- glm(  response.cw ~ folded_displacement
-               , binomial(link=logit.2asym(0.05, 0.05))
-               , subset(data, as.logical(responseInWindow))
-               )
+    formula <- update(formula, . ~ . + loaded_from)
+    cont$loaded_from <- "contr.sum"
   }
+   
+  fit <- glm(  formula
+             , binomial(link=logit.2asym(0.05, 0.05))
+             , subset(data, as.logical(responseInWindow))
+             , contrasts=if (length(cont) > 0) cont else NULL
+             )
   
   ##note this will find a bunch of intercepts (arg 2) if you want...
   cases <- data[1,,drop=FALSE]
   X <- find.intercept.glm(  fit, cases
-                                   , 'folded_displacement'
+                                   , var
                                    , response = c(.5, .75)
                                    , result.type="list"
                                    , sims=sims
                                    )
 
-    with(  X
+  with(  X
        , c(  xint = intercept[[1]]
-           , if (sims>0) c(xint = quantile(sim[[1]], c(0.1,0.25,0.50,0.75,0.9))) else c()
-           , if (sims>0) c(threshold = quantile(sim[[2]] - sim[[1]], c(0.1,0.25,0.50,0.75,0.9))) else c()
+           , if (sims>0) c(xint = quantile(sim[[1]], c(0.1,0.25,0.50,0.75,0.9), na.rm=TRUE)) else c()
+           , if (sims>0) c(threshold = quantile(sim[[2]] - sim[[1]], c(0.1,0.25,0.50,0.75,0.9), na.rm=TRUE)) else c()
            , threshold = intercept[[2]] - intercept[[1]]
-           , yint = fit$coefficients[[1]]
-           , yint.sd = sqrt(vcov(fit)[[1,1]])
-           , slope = fit$coefficients[[2]]
-           , slope.sd = sqrt(vcov(fit)[[2,2]])
-           , if (sims>0) slope.positive <- mean(sign(X$sim[[2]])) else c()
+           , if(one_sided) {
+               c( yint=0, yint.sd=0
+                 )
+             } else {
+               c(  yint = fit$coefficients[["(Intercept)"]]
+                 , yint.sd = ifelse(  !is.na(fit$coefficients[["(Intercept)"]])
+                                    , sqrt(vcov(fit)[["(Intercept)","(Intercept)"]])
+                                    , NA)
+                 )
+             }
+           
+           , slope = fit$coefficients[[var]]
+           , slope.sd = if(!is.na(fit$coefficients[[var]])) sqrt(vcov(fit)[[var,var]]) else NA
+           , if (sims>0) slope.positive <- mean(sim[[2]] - sim[[1]] > 0) else c()
            )
        )
 
