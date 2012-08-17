1b4007d9aa52f01c397ee5d032f70b2ede4a1ca7
peterm <peterm@2a468891-300a-0410-9f4f-8ca473c5017f>
2012-08-17 09:32:43 +0000

    version control is for deleting unused code
    git-svn-id: svn+ssh://pat.shadlen.org/home/peterm/svn/eyetracking/analysis/ConcentricDirectionQuest@875 2a468891-300a-0410-9f4f-8ca473c5017f

--- unused/backnforth.m
+++
@@ -1,4 +0,0 @@
-function b = backnforth(n)
-nn=1:n^2;
-full(sparse(ceil(nn/n),abs(mod(nn-0.5+n,2*n)-n)+0.5,nn))
-end
--- unused/mat2ascii.m
+++
@@ -1,6 +0,0 @@
-function mat2ascii(infile, outfile)
-
-s = load(infile);
-save(outfile, '-struct', 's', '-v6', '-ascii');
-
-end
