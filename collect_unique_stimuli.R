c5ad1ca8c6aad838f3522f99ad41dd71395ed316
Peter Meilstrup <peter.meilstrup@gmail.com>
2013-02-09 07:06:49 -0800

    generate motion energy grid...

--- collect_unique_stimuli.R
+++ collect_unique_stimuli.R
@@ -6,11 +6,12 @@ suppressPackageStartupMessages({
   library(R.matlab)
   library(stringr)
   source("db_functions.R")
+  source("helper_functions.R")
 })
 
 renamings <- c(
                trial_motion_process_radius   = "eccentricity",
-               abs_localDirectionContrast    = "direction_content",
+               abs_localDirectionContrast    = "abs_direction_content",
                trial_extra_content_ccw       = "content_ccw",
                trial_extra_content_cw        = "content_cw",
                abs_displacement              = "abs_displacement",
@@ -30,14 +31,71 @@ renamings <- c(
                trial_motion_process_n        = "n"
                )
 
-make_further_unique <- function(trials){
+phase_related_columns <- c("phase", "flanker1phase", "flanker2phase", "side")
+
+make_further_unique <- function(trials) {
   mutate(trials,
          phase = ifelse( is.na(flanker1phase) | is.na(flanker2phase),
                          NA, phase))
 }
 
-main <- function(dbfile, outfile, matfile, ...) {
-  listing_files <- c(...)
+expand <- function(trials) {
+  #In addition to all unique trials, get a sampled grid for motion energy
+  #calculating.
+  chain(trials,
+        subset((( target_number_shown == target_number_all)
+                | is.na(target_number_shown))
+               & ( wavelengthScalar == 0.075 )
+               & ( dt == .1 )
+               & ( eccentricity - 20/3 < 0.01 )
+               )) -> eligible
+
+  contents <- chain(eligible,
+                    .[!duplicated(.$abs_direction_content),],
+                    drop_columns(c("target_number_all", "abs_displacement")),
+                    unique)
+
+  spacings <- chain(eligible,
+                    subset(select="target_number_all"),
+                    unique)
+
+  #pick some displacment values...
+  displacements <-
+    chain(eligible,
+          ddply("abs_direction_content",
+                summarize,
+                range = diff(range(abs_displacement)),
+                count = length(unique(abs_displacement)),
+                spacing = min(diff(sort(unique(abs_displacement))))),
+          subset(spacing == max(spacing)),
+          subset(range == max(range)),
+          subset(count == max(count)),
+          .[1,],
+          match_df(eligible, .),
+          subset(select=c('abs_displacement'))
+        )
+
+  #this gives us a 3d grid to calculate motion energy over...
+  grid <- chain(contents,
+                merge(spacings),
+                merge(displacements),
+                mutate(grid=TRUE))
+
+  joint <- chain(trials,
+                 merge(grid, all.x=TRUE, all.y=TRUE),
+                 mutate(grid=ifelse(is.na(grid), FALSE, grid)))
+
+  joint
+}
+
+main <- function(dbfile="discrimination.sqlite",
+                 outfile="unique_stimuli.RData",
+                 matfile="unique_stimuli.mat", ...) {
+  if (missing(...)) {
+    listing_files <- Sys.glob("collections/*_series.list")
+  } else {
+    listing_files <- c(...)
+  }
 
   filenames <- chain(listing_files, lapply(readLines), unlist, str_trim, unique)
   drv <- SQLite()
@@ -47,15 +105,17 @@ main <- function(dbfile, outfile, matfile, ...) {
   chain(data,
         rename(renamings),
         make_further_unique,
-        unique) -> data
+        unique
+        ) -> data
 
-  data_without_phase <- data
-  data_without_phase[c("phase", "flanker1phase", "flanker2phase", "side")] <- list()
-  data_without_phase <- unique(data_without_phase)
+  chain(data,
+        drop_columns(phase_related_columns),
+        unique,
+        expand
+        ) -> data_without_phase
 
   save(data, data_without_phase, file=outfile)
   writeMat(con=matfile, data=data, data_without_phase = data_without_phase)
-
 }
 
 run_as_command()
--- helper_functions.R
+++ helper_functions.R
@@ -1,6 +1,10 @@
 #!/bin/env Rscript
 library(ptools)
 
+drop_columns <- function(data, drop) {
+  data[colnames(data)[!colnames(data) %in% drop]]
+}
+
 replace_extension <- function(filename, new_extension, append="") {
   sub(  "((.)\\.[^.]*|)$"
       , paste("\\2", append, ".", new_extension, sep="")
