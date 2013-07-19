fa485ae780dca8dae5bc9c95a8e92e5fb9708e8e
Peter Meilstrup <peter.meilstrup@gmail.com>
2013-07-19 07:16:17 -0700

    update parameter extraction

--- results.numbers.R
+++ results.numbers.R
@@ -1,12 +1,13 @@
 #here we compute some aggregate numbers about the stimuli from the database.
 
-#what is the eccentricity of the stimuli?
 library(plyr)
 library(stringr)
 source("db_functions.R")
 load("descriptions/descriptions.RData")
 drv <- SQLite()
 dbfile <- "discrimination.sqlite"
+
+#what is the resolusion and refresh rate?
 
 #answer questions about the trial set used in experiment 1
 main <- function(dbfile, outfile) {
@@ -23,33 +24,72 @@ main <- function(dbfile, outfile) {
 
   numbers <- list()
 
-  numbers$results.eccentricity <- unique(pull.from.sqlite(
-    dbfile,
-    data.frame(loaded_from=results.files),
-    columns="trial_motion_process_radius")[[1]])
+  calib <- local({
+    load("unpacked/pbm-2012-03-23__15-04-11-ConcentricAdjustmentPeriodic.RData")
+    with(runs, list(
+        resolution=beforeRun.params.cal.rect[[1]]
+        , size=(beforeRun.params.cal.spacing[[1]]
+                * beforeRun.params.cal.rect[[1]][c(3,4)])
+        , distance=beforeRun.params.cal.distance
+        , interval=beforeRun.params.cal.interval
+        , luminance=beforeRun.params.cal.calibration.stage2.readings[[1]]
+        , gamma=beforeRun.params.cal.gamma[[1]]
+        ))
+    })
+
+  #what are the resolutions, refresh rates, etc...
+  with.db.connection(drv, dbfile, fn=function(conn) {
+    df <- pull.from.db(
+      conn, data.frame(loaded_from=results.files),
+      columns=c(
+          "trial_extra_r", "trial_extra_min_distance", "trial_extra_dt",
+          "trial_motion_process_velocity", "trial_motion_process_wavelength",
+          "trial_motion_process_width", "trial_extra_tf",
+          "trial_motion_process_n",
+          "trial_extra_content_ccw", "trial_extra_content_cw",
+          "subject"))
+    numbers$dt <<- unique(df$trial_extra_dt)
+    numbers$tf <<- unique(with(df, abs(trial_extra_tf)))
+    numbers$sf <<- unique(1/df$trial_motion_process_wavelength)
+    numbers$width <<- unique(df$trial_motion_process_width)
+    numbers$eccentricity <<- unique(df$trial_extra_r)
+    #the underlying parameter is number of "steps"
+    numbers$n <<- unique(df$trial_motion_process_n + 1)
+    numbers$content <<- unique(with(
+        df, table(trial_extra_content_ccw + trial_extra_content_cw)))
+  })
+
 
   with.db.connection(drv, dbfile, fn=function(conn) {
     df <- pull.from.db(
       conn, data.frame(loaded_from=segment.files),
-      columns=c("trial_extra_flanker1Angle", "trial_extra_flanker2Angle",
-        "trial_extra_r", "trial_extra_min_distance", "trial_extra_nTargets",
-        "trial_extra_nVisibleTargets"))
-    numbers$min.flanker.angle <<- min(with(df, abs(trial_extra_flanker1Angle - trial_extra_flanker2Angle)))
-    numbers$max.flanker.angle <<- max(with(df, abs(trial_extra_flanker1Angle - trial_extra_flanker2Angle)))
-    numbers$min.distance <<- unique(with(df, trial_extra_min_distance * trial_extra_r))
-    numbers$tested <<- chain(df[c("trial_extra_nTargets",
-                                  "trial_extra_nVisibleTargets",
-                                  "trial_extra_r")],
-                             unique,
-                             mutate(spacing = 2*pi*trial_extra_r / trial_extra_nTargets,
-                                    radians = 2*pi/trial_extra_nTargets),
-                             merge(data.frame(trial_extra_nTargets=(c(12,21,15,15)),
-                                              trial_extra_nVisibleTargets = c(5,5,3,6),
-                                              selected=T),
-                                   all.x=T))})
-  numbers$segment.ecc <<- unique(df$trial_extra_r)
+      columns=c(
+          "trial_extra_flanker1Angle", "trial_extra_flanker2Angle",
+          "trial_extra_r", "trial_extra_min_distance", "trial_extra_nTargets",
+          "trial_extra_nVisibleTargets"))
+    numbers$min.flanker.angle <<-
+        min(with(df, abs(trial_extra_flanker1Angle -
+                         trial_extra_flanker2Angle)))
+    numbers$max.flanker.angle <<-
+        max(with(df, abs(trial_extra_flanker1Angle -
+                         trial_extra_flanker2Angle)))
+    numbers$min.distance <<-
+        unique(with(df, trial_extra_min_distance * trial_extra_r))
+    numbers$tested <<- chain(
+        df[c("trial_extra_nTargets",
+             "trial_extra_nVisibleTargets",
+             "trial_extra_r")],
+        unique,
+        mutate(spacing = 2*pi*trial_extra_r / trial_extra_nTargets,
+               radians = 2*pi/trial_extra_nTargets),
+        merge(data.frame(trial_extra_nTargets=(c(12,21,15,15)),
+                         trial_extra_nVisibleTargets = c(5,5,3,6),
+                         selected=T),
+              all.x=T))
+    numbers$segment.ecc <<- unique(df$trial_extra_r)
+  })
 
-  save(numbers, file=outfile)
+  save(numbers, calib, file=outfile)
 }
 
 run_as_command()
